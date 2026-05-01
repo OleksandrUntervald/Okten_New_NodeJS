1aa959a7ac8fa7d41872e1c41192c60ffab30a7d
OleksandrUntervald <untervaldthebest@gmail.com>
2026-05-01 06:09:43 +0300

    lesson3-hw

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -20,6 +20,18 @@ class UserController {
         const data = await userService.getById(id);
         res.status(StatusCodesEnum.OK).json(data)
     }
+
+    public async updateById(req: Request, res: Response) {
+        const id = req.params.id as string;
+        const user = req.body as IUserDTO;
+        const data = await userService.updateById(id, user);
+        res.status(StatusCodesEnum.OK).json(data)
+    }
+    public async deleteById(req: Request, res: Response) {
+        const id = req.params.id as string;
+        await userService.deleteById(id)
+        res.status(StatusCodesEnum.NO_CONTENT).end()
+    }
 }
 
 export const userController = new UserController();
