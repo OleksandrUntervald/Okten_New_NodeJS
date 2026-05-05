b6cd263a0cbf6a595bdce83f95b6e4d0422e29e6
OleksandrUntervald <untervaldthebest@gmail.com>
2026-05-05 21:59:51 +0300

    lesson4-cw

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,59 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import mongoose from "mongoose";
-import {config} from "./configs/config";
-import {apiRouter} from "./routers/api.router";
+
+import { config } from "./configs/config";
+import { ApiError } from "./errors/api.error";
+import { apiRouter } from "./routers/api.router";
 
 const app = express();
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use("/", apiRouter)
+app.use("/", apiRouter);
 
+// Error handler
+app.use((err: ApiError, req: Request, res: Response, _next: NextFunction) => {
+    const status = err.status || 500;
+    const message = err.message ?? "Something went wrong";
+    res.status(status).json({ status, message });
+});
 
-const dbConnection = async ()=>{
-    let dbCon = false
+process.on("uncaughtException", (err) => {
+    // eslint-disable-next-line no-console
+    console.log("uncaughtException", err);
+    process.exit(1);
+});
 
-    while (!dbCon){
+const dbConnection = async () => {
+    let dbCon = false;
+
+    while (!dbCon) {
         try {
-            console.log('Connecting to DB...');
-            await mongoose.connect(config.MONGO_URI)
-            dbCon =true
-            console.log('Database available!!!');
-        }catch (e) {
-            console.log('Database unavailable, wait 3 seconds');
-            await new Promise(resolve => setTimeout(resolve, 3000))
+            // eslint-disable-next-line no-console
+            console.log("Connecting to DB...");
+            await mongoose.connect(config.MONGO_URI);
+            dbCon = true;
+            // eslint-disable-next-line no-console
+            console.log("Database available!!!");
+        } catch {
+            // eslint-disable-next-line no-console
+            console.log("Database unavailable, wait 3 seconds");
+            await new Promise((resolve) => setTimeout(resolve, 3000));
         }
     }
-}
+};
 
-const start =async ()=>{
+const start = async () => {
     try {
-        await dbConnection()
-        app.listen(config.PORT,()=>{
+        await dbConnection();
+        app.listen(config.PORT, () => {
+            // eslint-disable-next-line no-console
             console.log(`Server listening on ${config.PORT}`);
-        })
-    }catch (e) {
+        });
+    } catch (e) {
+        // eslint-disable-next-line no-console
         console.log(e);
     }
-}
+};
 
-start()
+void start();
