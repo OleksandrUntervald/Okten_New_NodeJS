395881b756438a571b1050b8225e20015d8f7dd2
OleksandrUntervald <untervaldthebest@gmail.com>
2026-05-13 21:25:07 +0300

    lesson6-hw

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,10 +1,6 @@
 import { StatusCodesEnum } from "../enums/status-codes.enum";
 import { ApiError } from "../errors/api.error";
-import {
-    IUser,
-    IUserCreateDTO,
-    IUserUpdateDTO,
-} from "../interfaces/user.interface";
+import { IUser, IUserCreateDTO } from "../interfaces/user.interface";
 import { userRepository } from "../repositories/user.repository";
 
 class UserService {
@@ -28,7 +24,7 @@ class UserService {
 
     public async updateById(
         userId: string,
-        user: IUserUpdateDTO,
+        user: Partial<IUser>,
     ): Promise<IUser> {
         const data = await userRepository.getById(userId);
 
@@ -71,6 +67,9 @@ class UserService {
 
     public unBlockUser(user_id: string): Promise<IUser> {
         return userRepository.unBlockUser(user_id);
+    }
+    public getByEmail(email: string): Promise<IUser> {
+        return userRepository.getByEmail(email);
     }
 }
 
